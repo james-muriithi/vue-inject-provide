e58f7a63e20fa2ee0abc948d32b273d6452a29ee
James Muriithi <muriithijames556@gmail.com>
2021-03-19 20:50:11 +0300

    initial commit

---
+++ src/components/KnowledgeGrid.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+      @select-topic="$emit('select-topic', $event)"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  inject: ['topics']
+};
+</script>
